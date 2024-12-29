eccbdcb28327306f5a9e2c46ca96657a9ea55174
vig-nesh-waran <vigneshwarans1102@gmail.com>
2024-12-29 16:20:48 +0530

    Sticky Notes app published

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: "/stickyNotes/",
 });
